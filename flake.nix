8622321e2b4827a0f880d7f29f4cf9852848b71d
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2026-01-03 17:02:46 -0500

    feat: nix flakes

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A tool for adding ruby annotations to fonts.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        craneLib = crane.mkLib pkgs;
+
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+        };
+
+        rubify = craneLib.buildPackage (
+          commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          }
+        );
+      in
+      {
+        packages.default = rubify;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = rubify;
+        };
+      }
+    );
+}
